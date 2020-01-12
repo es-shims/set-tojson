6871facbb319dbdfc790042bbea87edbf743139a
Jordan Harband <ljharb@gmail.com>
2020-01-12 14:32:11 -0800

    [Refactor] use `iterate-value` to iterate the Set

--- index.js
+++ index.js
@@ -3,33 +3,11 @@
 var IsCallable = require('es-abstract/2019/IsCallable');
 var RequireObjectCoercible = require('es-abstract/2019/RequireObjectCoercible');
 var define = require('define-properties');
+var iterate = require('iterate-value');
+var callBound = require('es-abstract/helpers/callBound');
+var $setSize = callBound('%Set.prototype.size', true);
 
 var hasSets = typeof Set !== 'undefined' && IsCallable(Set);
-
-var setValues;
-if (hasSets) {
-	setValues = Set.prototype.values;
-}
-var push = Array.prototype.push;
-
-// polyfilled Sets with es6-shim might exist without for..of
-var iterateWithWhile = function (set, receive) {
-	var values = setValues.call(set);
-	var next;
-	do {
-		next = values.next();
-	} while (!next.done && receive(next.value));
-};
-
-var iterate = (function () {
-	try {
-		// Safari 8's native Set can't be iterated except with for..of
-		return Function('setValues', 'set', 'receive', 'for (var value of setValues.call(set)) { receive(value); }').bind(null, setValues);
-	} catch (e) {
-		/* for..of seems to not be supported */
-	}
-	return iterateWithWhile;
-}());
 
 var requireSet = function requireGlobalSet() {
 	if (!hasSets) {
@@ -40,9 +18,8 @@ var requireSet = function requireGlobalSet() {
 var setToJSONshim = function toJSON() {
 	RequireObjectCoercible(this);
 	requireSet();
-	var values = [];
-	iterate(this, push.bind(values));
-	return values;
+	$setSize(this);
+	return iterate(this);
 };
 
 var boundSetToJSON = function setToJSON(set) {
